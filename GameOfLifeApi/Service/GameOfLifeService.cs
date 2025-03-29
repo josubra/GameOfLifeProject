0b0b0c13c863ecf5546560210214b03b16ca2025
josubra <josueftbr@gmail.com>
2025-03-29 11:10:38 -0300

    Adding some more features - Added repository Pattern - Added Service layer - Added first endpoint to get Last Board State

---
+++ GameOfLifeApi/Service/GameOfLifeService.cs
@@ -0,0 +1,21 @@
+﻿using GameOfLifeApi.Data.Repository.Abstraction;
+using GameOfLifeApi.Entity;
+using GameOfLifeApi.Service.Abstractions;
+using System.Linq.Expressions;
+
+namespace GameOfLifeApi.Service
+{
+    public class GameOfLifeService : IGameOfLifeService
+    {
+        private readonly IRepository<GameOfLifeBoard> _repository;
+        public GameOfLifeService(IRepository<GameOfLifeBoard> repository) 
+        {
+            _repository = repository;
+        }
+
+        public async Task<GameOfLifeBoard> GetGameOfLifeBoardAsync(int id, CancellationToken cancellationToken = default)
+        {
+            return await _repository.GetByIdAsync(id, cancellationToken);
+        }
+    }
+}
