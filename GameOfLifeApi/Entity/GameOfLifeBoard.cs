1cd9e90d049243a242c0c7e23dbf590f2b0b3565
josubra <josueftbr@gmail.com>
2025-03-29 09:45:07 -0300

    features: - add docker compose to manage the project containerization. - add mysql container - add Entity Framework Dependencies - Configure Entity Framework - Add initial migrations for GameOfLifeBoard Entity

---
+++ GameOfLifeApi/Entity/GameOfLifeBoard.cs
@@ -0,0 +1,9 @@
+﻿namespace GameOfLifeApi.Entity
+{
+    public class GameOfLifeBoard 
+    {
+        public int Id { get; set; }
+
+        public string LastBoardState { get; set; } = string.Empty;
+    }
+}
