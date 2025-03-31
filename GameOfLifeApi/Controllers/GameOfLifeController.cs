29faf80b648d0c2794490687f8570afdca080aa4
josubra <josueftbr@gmail.com>
2025-03-31 15:47:41 -0300

    Added new features: - Logging for Web API and Service - Input validation - Global error Handling

--- GameOfLifeApi/Controllers/GameOfLifeController.cs
+++ GameOfLifeApi/Controllers/GameOfLifeController.cs
@@ -1,6 +1,7 @@
 using GameOfLifeApi.Data;
 using GameOfLifeApi.Entity;
 using GameOfLifeApi.Service.Abstractions;
+using GameOfLifeApi.Util;
 using Microsoft.AspNetCore.Mvc;
 
 namespace GameOfLifeApi.Controllers
@@ -19,9 +20,13 @@ namespace GameOfLifeApi.Controllers
             _gameOfLifeService = gameOfLifeService;
         }
 
+
         [HttpGet("/board")]
         public async Task<ActionResult<GameOfLifeBoard>> Get(int id, CancellationToken cancelationToken)
         {
+            var validationResult = ValidateAndLog(id, guid => id > 0, "Invalid board Id received.");
+            if (validationResult != null) return validationResult;
+
             var board = await _gameOfLifeService.GetGameOfLifeBoardAsync(id, cancelationToken);
             if (board == null)
                 return NotFound();
@@ -29,15 +34,22 @@ namespace GameOfLifeApi.Controllers
         }
 
         [HttpPost("/board")]
-        public async Task<int> Post([FromBody] bool[][] boardState, CancellationToken cancelationToken)
+        public async Task<ActionResult> Post([FromBody] bool[][] boardState, CancellationToken cancelationToken)
         {
+            var validationResult = ValidateAndLog(boardState,HelperUtil.ValidateJaggedArray,
+                 "Invalid board state received.");
+            if (validationResult != null) return validationResult;
+
             var newBoard = await _gameOfLifeService.AddGameOfLifeBoardAsync(boardState, cancelationToken);
-            return newBoard.Id;
+            return Ok(new { boardId = newBoard.Id });
         }
 
         [HttpGet("/board/{id}/next")]
         public async Task<ActionResult<bool[][]?>> GetNextState(int id, CancellationToken cancelationToken)
         {
+            var validationResult = ValidateAndLog(id, guid => id > 0, "Invalid board Id received.");
+            if (validationResult != null) return validationResult;
+
             var nextState = await _gameOfLifeService.GetAndSaveNextStateAsync(id);
             return nextState != null ? Ok(nextState) : NotFound();
         }
@@ -45,6 +57,10 @@ namespace GameOfLifeApi.Controllers
         [HttpGet("/board/{id}/next/{steps}")]
         public async Task<ActionResult<bool[][]?>> GetFutureState(int id, int steps, CancellationToken cancelationToken)
         {
+            var validationResult = ValidateAndLog((id, steps), data => data.id > 0 && data.steps > 0,
+                $"Invalid parameters for next state. Parameters must be greater than zero.");
+            if (validationResult != null) return validationResult;
+
             var nextState = await _gameOfLifeService.GetAndSaveFutureStateAsync(id, steps);
             return nextState != null ? Ok(nextState) : NotFound();
         }
@@ -52,8 +68,23 @@ namespace GameOfLifeApi.Controllers
         [HttpGet("/board/{id}/final/{maxAttempts}")]
         public async Task<ActionResult<bool[][]?>> GetFinalState(int id, int maxAttempts, CancellationToken cancelationToken)
         {
+            var validationResult = ValidateAndLog((id, maxAttempts), data => data.id > 0 && data.maxAttempts > 0,
+                $"Invalid parameters for next state. Parameters must be greater than zero.");
+            if (validationResult != null) return validationResult;
+
             var finalState = await _gameOfLifeService.GetAndSaveFinalStateAsync(id, maxAttempts);
             return finalState != null ? Ok(finalState) : NotFound();
+        }
+
+        //In a big project, this method could be moved to a base controller class
+        private ActionResult? ValidateAndLog<T>(T input, Func<T, bool> isValid, string errorMessage)
+        {
+            if (!isValid(input))
+            {
+                _logger.LogWarning(errorMessage);
+                return BadRequest(errorMessage);
+            }
+            return null;
         }
     }
 }
---
+++ GameOfLifeApi/Exception/ErrorResponse.cs
@@ -0,0 +1,11 @@
+﻿namespace GameOfLifeApi.Exception
+{
+    public class ErrorResponse
+    {
+        public string? Endpoint { get; set; }
+        public int StatusCode { get; set; }
+        public string? Title { get; set; }
+        public string? ExceptionMessage { get; set; }
+        public string? StackTrace { get; set; }
+    }
+}
