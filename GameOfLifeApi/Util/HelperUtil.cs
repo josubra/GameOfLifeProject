b7c45ebbed84fa137077ed2ce6dec864f04a1c65
josubra <josueftbr@gmail.com>
2025-04-01 14:46:06 -0300

    - Added integrated tests for database using Testcontainers. Unit tests: - Added tests to GameOfLifeService - Added tests to Helper Methods the calculate next state of board, check valid board, countLivingNeighbots and compare boards.

--- GameOfLifeApi/Util/HelperUtil.cs
+++ GameOfLifeApi/Util/HelperUtil.cs
@@ -1,8 +1,11 @@
-﻿namespace GameOfLifeApi.Util
+﻿using GameOfLifeApi.Entity;
+using System.Text.Json;
+
+namespace GameOfLifeApi.Util
 {
     public static class HelperUtil
     {
-        public static bool[][] GenerateFirstBoard(int rows, int columns)
+        public static bool[][] GenerateBoard(int rows, int columns)
         {
             var board = new bool[rows][];
             for (int i = 0; i < rows; i++)
@@ -16,7 +19,7 @@
             return board;
         }
 
-        public static bool ValidateJaggedArray(bool[][] jaggedArray)
+        public static bool ValidateBoard(bool[][] jaggedArray)
         {
             if (jaggedArray == null || jaggedArray.Length < 3)
                 return false;
@@ -29,6 +32,134 @@
                     return false;
             }
 
+            return true;
+        }
+
+        public static bool[][] SampleBoard => new bool[][]
+        {
+            new bool[] { false, true, false },
+            new bool[] { false, true, false },
+            new bool[] { false, true, false }
+        };
+
+        public static bool[][] EmptyBoard => new bool[][]
+        {
+            new bool[] { false, false, false },
+            new bool[] { false, false, false },
+            new bool[] { false, false, false }
+        };
+
+        public static bool[][] BlockBoard = new bool[][]
+        {
+            new bool[] { false, false, false, false },
+            new bool[] { false, true,  true,  false },
+            new bool[] { false, true,  true,  false },
+            new bool[] { false, false, false, false }
+        };
+
+        public static bool[][] BlinkerBoard1 = new bool[][]
+        {
+            new bool[] { false, false, false, false, false },
+            new bool[] { false, false, true,  false, false },
+            new bool[] { false, false, true,  false, false },
+            new bool[] { false, false, true,  false, false },
+            new bool[] { false, false, false, false, false }
+        };
+
+        public static bool[][] BlinkerBoard2 = new bool[][]
+        {
+            new bool[] { false, false, false, false, false },
+            new bool[] { false, false, false, false, false },
+            new bool[] { false, true,  true,  true,  false },
+            new bool[] { false, false, false, false, false },
+            new bool[] { false, false, false, false, false }
+        };
+
+        public static bool[][] SingleCellBoard = new bool[][]
+        {
+            new bool[] { false, false, false },
+            new bool[] { false, true,  false },
+            new bool[] { false, false, false }
+        };
+
+        public static bool[][] InvalidBoard => new bool[][]
+        {
+            new bool[] { false, true },
+            new bool[] { false },
+            new bool[] { false, true, false }
+        };
+
+        public static GameOfLifeBoard CreateTestBoard(int id, bool[][] boardState)
+        {
+            return new GameOfLifeBoard
+            {
+                Id = id,
+                LastBoardState = JsonSerializer.Serialize(boardState)
+            };
+        }
+
+        public static bool[][] ComputeNextState(bool[][] board)
+        {
+            int rows = board.Length;
+            int cols = board[0].Length;
+            bool[][] newBoard = new bool[rows][];
+            for (int r = 0; r < rows; r++)
+            {
+                newBoard[r] = new bool[cols];
+                for (int c = 0; c < cols; c++)
+                {
+                    int liveNeighbors = CountLiveNeighbors(board, r, c);
+                    if (board[r][c])
+                    {
+                        newBoard[r][c] = liveNeighbors == 2 || liveNeighbors == 3;
+                    }
+                    else
+                    {
+                        newBoard[r][c] = liveNeighbors == 3;
+                    }
+                }
+            }
+            return newBoard;
+        }
+
+        public static int CountLiveNeighbors(bool[][] board, int row, int col)
+        {
+            int rows = board.Length;
+            int cols = board[0].Length;
+            int count = 0;
+
+            for (int dr = -1; dr <= 1; dr++)
+            {
+                for (int dc = -1; dc <= 1; dc++)
+                {
+                    if (dr == 0 && dc == 0) continue;
+                    int newRow = row + dr;
+                    int newCol = col + dc;
+                    if (newRow >= 0 && newRow < rows && newCol >= 0 && newCol < cols && board[newRow][newCol])
+                    {
+                        count++;
+                    }
+                }
+            }
+            return count;
+        }
+
+        public static bool AreBoardsEqual(bool[][] board1, bool[][] board2)
+        {
+            int rowsBoard1 = board1.Length;
+            int colsBoard1 = board1[0].Length;
+            int rowsBoard2 = board2.Length;
+            int colsBoard2 = board2[0].Length;
+
+            if (rowsBoard1 != rowsBoard2 || colsBoard1 != colsBoard2) return false;
+
+            for (int r = 0; r < rowsBoard1; r++)
+            {
+                for (int c = 0; c < colsBoard1; c++)
+                {
+                    if (board1[r][c] != board2[r][c]) return false;
+                }
+            }
             return true;
         }
     }
---
+++ GameOfLifeTests/HelperUtilTests.cs
@@ -0,0 +1,179 @@
+﻿using DotNet.Testcontainers;
+using GameOfLifeApi.Util;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameOfLifeTests
+{
+    public class HelperUtilTests
+    {
+        [Fact]
+        public void CountLiveNeighbors_EmptyBoard_ReturnsZero()
+        {
+            var board = HelperUtil.EmptyBoard;
+
+            for (int r = 0; r < board.Length; r++)
+            {
+                for (int c = 0; c < board[0].Length; c++)
+                {
+                    var count = HelperUtil.CountLiveNeighbors(board, r, c);
+                    Assert.Equal(0, count);
+                }
+            }
+        }
+
+        [Fact]
+        public void CountLiveNeighbors_CenterOfBlock_ReturnsThree()
+        {
+            var board = HelperUtil.BlockBoard;
+            int centerRow = 1;
+            int centerColumn = 1;
+
+            var count = HelperUtil.CountLiveNeighbors(board, centerRow, centerColumn);
+
+            Assert.Equal(3, count);
+        }
+
+        [Fact]
+        public void CountLiveNeighbors_CornerCell_ReturnsCorrectCount()
+        {
+            var board = HelperUtil.BlockBoard;
+            int cornerRow = 0;
+            int cornerColumn = 0;
+
+            var count = HelperUtil.CountLiveNeighbors(board, cornerRow, cornerColumn);
+
+            Assert.Equal(1, count);
+        }
+
+        [Fact]
+        public void ComputeNextState_EmptyBoard_RemainsEmpty()
+        {
+            var board = HelperUtil.EmptyBoard;
+
+            var nextState = HelperUtil.ComputeNextState(board);
+
+            Assert.True(HelperUtil.AreBoardsEqual(board, nextState));
+        }
+
+        [Fact]
+        public void ComputeNextState_BlockBoard_RemainsUnchanged()
+        {
+            var board = HelperUtil.EmptyBoard;
+
+            var nextState = HelperUtil.ComputeNextState(board);
+
+            Assert.True(HelperUtil.AreBoardsEqual(board, nextState));
+        }
+
+        [Fact]
+        public void ComputeNextState_Blinker_OscillatesCorrectly()
+        {
+            var phase1 = HelperUtil.BlinkerBoard1;
+            var phase2 = HelperUtil.BlinkerBoard2;
+
+            var computedPhase2 = HelperUtil.ComputeNextState(phase1);
+            var computedPhase1 = HelperUtil.ComputeNextState(phase2);
+
+            Assert.True(HelperUtil.AreBoardsEqual(phase2, computedPhase2));
+            Assert.True(HelperUtil.AreBoardsEqual(phase1, computedPhase1));
+        }
+
+        [Fact]
+        public void ComputeNextState_SingleCell_Dies()
+        {
+            var board = HelperUtil.SingleCellBoard;
+
+            var nextState = HelperUtil.ComputeNextState(board);
+
+            Assert.True(HelperUtil.AreBoardsEqual(HelperUtil.EmptyBoard, nextState));
+        }
+
+        [Fact]
+        public void CheckInvalidBoard_ShouldReturnFalse()
+        {
+            var board = HelperUtil.InvalidBoard;
+
+            var isValid = HelperUtil.ValidateBoard(board);
+
+            Assert.False(isValid);
+        }
+
+        [Fact]
+        public void CheckValidBoard_ShouldReturnTrue()
+        {
+            var board = HelperUtil.BlockBoard;
+
+            var isValid = HelperUtil.ValidateBoard(board);
+
+            Assert.True(isValid);
+        }
+
+        [Fact]
+        public void AreBoardsEqual_IdenticalBoards_ReturnsTrue()
+        {
+            var board1 = HelperUtil.BlockBoard;
+            var board2 = HelperUtil.BlockBoard.Select(row => row.ToArray()).ToArray();
+
+            var result = HelperUtil.AreBoardsEqual(board1, board2);
+
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void AreBoardsEqual_DifferentBoards_ReturnsFalse()
+        {
+            var board1 = HelperUtil.BlockBoard;
+            var board2 = HelperUtil.SingleCellBoard;
+
+            var result = HelperUtil.AreBoardsEqual(board1, board2);
+
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void AreBoardsEqual_SingleDifference_ReturnsFalse()
+        {
+            var board1 = HelperUtil.BlockBoard;
+            var board2 = HelperUtil.BlockBoard.Select(row => row.ToArray()).ToArray();
+            board2[1][1] = !board2[1][1];
+
+            var result = HelperUtil.AreBoardsEqual(board1, board2);
+
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void CountLiveNeighbors_AllLive_Returns8ForCenter()
+        {
+            var board = new bool[][]
+            {
+                new bool[] { true, true, true },
+                new bool[] { true, true, true },
+                new bool[] { true, true, true }
+            };
+
+            var count = HelperUtil.CountLiveNeighbors(board, 1, 1);
+
+            Assert.Equal(8, count);
+        }
+
+        [Fact]
+        public void ComputeNextState_Overpopulation_Dies()
+        {
+            var board = new bool[][]
+            {
+                new bool[] { true, true, true },
+                new bool[] { true, true, true },
+                new bool[] { false, false, false }
+            };
+
+            var nextState = HelperUtil.ComputeNextState(board);
+
+            Assert.False(nextState[1][1]); // Center cell dies from overpopulation
+        }
+    }
+}
